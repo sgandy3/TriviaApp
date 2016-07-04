3636d169c0a0ee529bd69efd99b6e0602a8a79e5
sgandy3 <sgandy3@yahoo.com>
2016-07-04 13:03:44 +0900

    Project's Initial Commit

---
+++ www/script/index.js
@@ -0,0 +1,22 @@
+ 
+/*var seconds = 60;
+function secondPassed() {
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;  
+    }
+    document.getElementById("clock").innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById("clock").innerHTML = "Buzz Buzz";
+    } else {
+        seconds--;
+    }
+}
+ 
+var countdownTimer = setInterval('secondPassed()', 1000);
+
+*/
+
+
